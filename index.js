69747072196909267d04438ca13e1cb9b8a1bfe9
Niko <nikoheikkilat@gmail.com>
2022-05-12 23:10:23 +0300

    Music Player fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('node:fs');
 const {Client, Collection, Intents} = require('discord.js');
 const {token} = require('./config.json');
+const { Player } = require("discord-player");
 
 // Create new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS , "GUILD_VOICE_STATES"]});
