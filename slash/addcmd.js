34c8f81abd551b2a5869cb1fc8587e9c12f4530f
Niko <nikoheikkilat@gmail.com>
2022-05-14 11:43:50 +0300

    Fixes

--- slash/addcmd.js
+++ slash/addcmd.js
@@ -7,24 +7,14 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName("addcmd")
     .setDescription("add custom commands")
-    .addSubcommand((subcommand) => 
-    subcommand.setName("new")
-        .setDescription("Makes a new custom command")
-        .addStringOption((option) => option.setName("name").setDescription("commands name").setRequired(true))
-        .addStringOption((option) => option.setName("content").setDescription("commands content").setRequired(true))
-    ),
+    .addStringOption((option) => option.setName("name").setDescription("commands name").setRequired(true))
+    .addStringOption((option) => option.setName("content").setDescription("commands content").setRequired(true)),
 
     run: (client, message, args, interaction) => {
 
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: You need `MANAGE_MESSAGES` permissions to use this command")
-
         let cmdName = interaction.options.getString("name")
-
-        if (!cmdName) return message.channel.send(`:x: You have to give a command name, \`addcmd <cmd_name> <cmd_content>\``)
         
         let cmdResponse = interaction.options.getString("content")
-
-        if (!cmdResponse) return message.channel.send(`:x: You have to give command content, \`addcmd <cmd_name> <cmd_content>\``)
 
         let database = db.get(`${message.guild.id}`)
 
